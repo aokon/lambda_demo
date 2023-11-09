bc82f56599e6ee775290c3093d5d88d201399adc
Adam Okoń <adam.okon@elegantcode.pl>
2023-11-09 12:46:26 +0100

    make labda setup in structure way

--- lib/lambda_demo.rb
+++ lib/lambda_demo.rb
@@ -1,16 +1,25 @@
 # frozen_string_literal: true
 
 require "bundler"
-
-stages = [:default]
-stages.push(:development) if ENV["LAMBDA_ENV"] == "development"
-
-Bundler.require(*stages)
-
-loader = Zeitwerk::Loader.new
-loader.push_dir(__dir__)
-loader.setup
+require "zeitwerk"
 
 module LambdaDemo
   VERSION = "1.0"
+
+  def self.setup_zeitwerk
+    loader = Zeitwerk::Loader.new
+    loader.tag = "lambda-demo"
+    loader.push_dir(__dir__)
+    loader.setup
+  end
+
+  def self.setup_dependencies
+    stages = [:default]
+    stages.push(:development) if ENV["LAMBDA_ENV"] == "development"
+
+    Bundler.require(*stages)
+  end
 end
+
+LambdaDemo.setup_zeitwerk
+LambdaDemo.setup_dependencies
